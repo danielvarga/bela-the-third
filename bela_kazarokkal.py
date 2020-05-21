bf0e8586c7432818818aaf9478cb5a58b9ad673f
Daniel Varga <daniel.varga@prezi.com>
2020-05-21 17:09:32 +0200

    first and last commit

---
+++ bela_kazarokkal.py
@@ -0,0 +1,137 @@
+import sys
+import pandas as pd
+import xgboost as xgb
+from sklearn.preprocessing import LabelEncoder
+import sklearn.svm
+import numpy as np
+from xgboost import plot_tree
+import sklearn.manifold
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+def logg(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
+
+colorblind_color_cycle = ['#377eb8', '#ff7f00', '#4daf4a',
+                  '#f781bf', '#a65628', '#984ea3',
+                  '#999999', '#e41a1c', '#dede00']
+
+from cycler import cycler
+mpl.rcParams['axes.prop_cycle'] = cycler(color=colorblind_color_cycle) # cycler(color='bgrcmyk')
+
+
+# source = "szerb.tsv"
+# source = "bela.tsv"
+# source = "bela_tigris.tsv"
+# source = "bela_merged.tsv"
+# source = "bela_kazarokkal.tsv"
+source = sys.argv[1]
+if len(sys.argv)>2:
+    np.random.seed(int(sys.argv[2]))
+
+data = pd.read_csv(source, sep='\t')
+
+standardize = False
+if standardize:
+    markers = data.iloc[:, 2:]
+    markers_std = markers.std()
+    data.iloc[:, 2:] = (markers - markers.mean()) / markers_std
+    logg("standardized data, marker stds:")
+    logg(markers_std)
+
+label_column = "Haplogroup"
+index_column = "Kit Number"
+
+belaek_count = 1
+
+belaek = data.iloc[:belaek_count]
+belaek_names = belaek[index_column]
+belaek = belaek.drop([label_column, index_column], axis=1)
+
+data = data.iloc[belaek_count:]
+
+le = LabelEncoder()
+data[label_column] = le.fit_transform(data[label_column])
+logg(np.unique(data[label_column], return_counts=True))
+
+n = len(data)
+
+data = data.sample(frac=1)
+
+train_size = 90*n//100
+train = data.iloc[:train_size]
+test = data.iloc[train_size:]
+
+logg("train on", len(train), "test on", len(test))
+
+balance = False
+if balance:
+    grouped = train.groupby(label_column)
+    train = grouped.apply(lambda x: x.sample(grouped.size().max(), replace=True)).reset_index(drop=True)
+    print("rebalanced, now train on", len(train), "test still on", len(test))
+
+train_X = train.drop([label_column, index_column], axis=1)
+train_y = train[label_column]
+test_X = test.drop([label_column, index_column], axis=1)
+test_y = test[label_column]
+
+labels = np.array(le.inverse_transform(range(np.max(train_y)+1)))
+
+
+do_tsne = False
+if do_tsne:
+    embedder = sklearn.manifold.TSNE(n_components=2, perplexity=30, metric='l2') # l1 also makes some sense.
+    # embedder = sklearn.decomposition.PCA(n_components=2, whiten=True)
+    train_and_belaek = np.concatenate((train_X, belaek))
+    reduced = embedder.fit_transform(train_and_belaek)
+    for i, l in enumerate(labels):
+        plt.scatter(reduced[:-len(belaek)][train_y==i, 0], reduced[:-len(belaek)][train_y==i, 1], label=l)
+    print("unimplemented Belaek vis")
+    # plt.scatter(reduced[-1, 0], reduced[-1, 1], marker='s', c='r', s=100, label="III. Béla")
+    plt.legend()
+
+    plt.savefig("tsne.png")
+    plt.show()
+    sys.exit()
+
+
+# that was good for bela.tsv:
+#classifier = xgb.XGBClassifier(max_depth=7, n_estimators=1000, learning_rate=0.05)
+# that's tuned for bela_tigris.tsv:
+classifier = xgb.XGBClassifier(max_depth=7, n_estimators=1000)
+# classifier = sklearn.svm.SVC(kernel='linear', C=1000)
+
+classifier = classifier.fit(train_X, train_y)
+
+predictions = classifier.predict(test_X)
+
+logg("test gold histogram", np.unique(test_y, return_counts=True))
+logg("majority baseline", np.max(np.bincount(test_y)) / len(test_y))
+logg("prediction histogram", np.unique(predictions, return_counts=True))
+
+acc = float((predictions == test_y).sum()) / len(test_y)
+
+print("acc", acc)
+
+for i, name in enumerate(belaek_names):
+    bela = belaek[i:i+1]
+    print(name, "haplogroup:", le.inverse_transform(classifier.predict(bela)[0]))
+
+    bela_y = classifier.predict_proba(bela)
+
+    logg(le.inverse_transform(range(np.max(train_y)+1)))
+    np.set_printoptions(suppress=True)
+    print(name, "haplogroup probs:", bela_y)
+
+# xgb.plot_importance(classifier)
+# plt.show()
+
+vis = False
+if vis:
+    plot_tree(classifier, num_trees=2) # 2 is Z280
+    fig = plt.gcf()
+    fig.set_size_inches(75, 50)
+    plt.savefig("bela.Z280.tree0.png")
